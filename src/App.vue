7dabb79de398ccabae2b4110c8bf647aa27c3f75
Thebigbot <77632836+Thebigbot0000@users.noreply.github.com>
2022-07-10 09:27:37 +0200

    ...

--- src/App.vue
+++ src/App.vue
@@ -3,19 +3,20 @@ import Navbar from './components/Navbar.vue';
 import WorkArea from './components/WorkArea/WorkAreaDefault.vue';
 
 export default {
-    components: {
-        Navbar,
-        WorkArea
+    data() {
+        return {
+            page: 0
+        }
     }
 }
 </script>
 
 <template>
-    <div class="navbar">
-        <Navbar />
+    <div class="home" v-if="page == 0">
+
     </div>
-    <div class="workarea">
-        <WorkArea />
+    <div class="workarea" v-else-if="page == 1">
+
     </div>
 </template>
 
